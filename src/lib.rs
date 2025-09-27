77c1eadaca3f2aebf40dc412df75b70c5493e286
JDPlumbing <jdplumbingsoflo@gmail.com>
2025-09-27 17:58:10 -0400

    flattened structure

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,13 @@
-pub mod sim; // top-level simulation orchestration
+pub mod chronovox; // event timelines
+pub mod droidid;   // droid identification
+pub mod geospec;   // geometry & shapes
+pub mod matcat;    // material catalog
+pub mod objex;     // object representation
+pub mod shortid;   // short ID generation
+pub mod sim;       // simulation orchestrator
+pub mod supabasic; // basic Supabase client
+pub mod tdt;       // time delta utils
+pub mod uvoxid;    // spatial ID system
+pub mod uvoxxyz;   // coordinate utils
+
+
